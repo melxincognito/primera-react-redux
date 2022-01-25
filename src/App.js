b99c841ff03a60f015578554039fe98a7f0a992d
Mel Incognito <melxincognito@gmail.com>
2022-01-25 16:20:55 -0700

    only displaying account from object

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import "./App.css";
 
 function App() {
-  const state = useSelector((state) => state);
+  const state = useSelector((state) => state.account); // if you just want the account you put state.account and it'll only display the account
 
   console.log(state);
   return <div className="App"></div>;
