7848c28f8ac2280ec337ade6a20b6a715f82427a
Mel Incognito <melxincognito@gmail.com>
2022-01-25 16:43:32 -0700

    added action creators

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/index";
 import "./App.css";
 
 function App() {
   const state = useSelector((state) => state.account); // if you just want the account you put state.account and it'll only display the account
+  const dispatch = useDispatch();
 
-  console.log(state);
+  const AC = bindActionCreators(actionCreators, dispatch);
+  console.log(AC);
+
   return <div className="App"></div>;
 }
 
