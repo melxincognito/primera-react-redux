2f46b1bea345c5a1d9783954381a789cbfaa3684
Mel Incognito <melxincognito@gmail.com>
2022-01-25 20:28:26 -0700

    changing the state on a simple counter app

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import { actionCreators } from "./state/index";
 import "./App.css";
 
 function App() {
-  const state = useSelector((state) => state.account); // if you just want the account you put state.account and it'll only display the account
+  const account = useSelector((state) => state.account); // if you just want the account you put state.account and it'll only display the account
+
   const dispatch = useDispatch();
 
-  const AC = bindActionCreators(actionCreators, dispatch);
-  console.log(AC);
+  const { depositMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+  // you can destructure out deposit and withdraw from your actionCreators that you binded in this const
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1> {account}</h1>
+      <button onClick={() => depositMoney(1)}> Deposit </button>
+      <button onClick={() => withdrawMoney(1)}> Withdraw </button>
+    </div>
+  );
 }
 
 export default App;
--- src/state/store.js
+++ src/state/store.js
@@ -1,4 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import reducers from "./reducers/index";
 
-export const store = createStore(reducers, {});
+export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+// if you don't apply the middleware with thunk it won't make the connection
+// to go though when you call it in a function
+
+// import thunk from redux thunk and applyMiddleware from redux
